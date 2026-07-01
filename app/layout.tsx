30df3e7cc5afd808be8beb3637783b8976690c17
GNAUTO <info@getnewautocare.com.au>
2026-07-01 19:54:41 +1000

    add Microsoft Clarity tracking script to layout head

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,16 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4054288002830029"
           crossOrigin="anonymous"
         />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "xfl2p795s0");`,
+          }}
+        />
       </head>
       <body className={geist.className}>
         {children}
